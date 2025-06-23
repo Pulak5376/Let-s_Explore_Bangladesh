7f426afa3c7b755dc473372d6c3ed962dda4f06e
turzo1061 <turzo15-5785@diu.edu.bd>
2025-06-23 23:39:11 +0600

    css and welcome page modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,14 @@
-<!-- welcome.blade.php -->
+<!-- resources/views/welcome.blade.php -->
 @extends('layout.master')
+
 @section('title', 'Welcome')
+
 @section('content')
 <section class="welcome-section">
     <div class="welcome-banner">
-        <h1>Let's Explore Bangladesh</h1>
-        <p>Discover the land of rivers, culture, and hospitality</p>
-        <a href="/home" class="btn">Start Exploring</a>
+        <h1 class="headline">Explore the Beauty of Bangladesh</h1>
+        <p class="subtext">Discover the land of rivers, vibrant culture, and warm hospitality.</p>
+        <a href="{{ url('/home') }}" class="btn btn-primary">Start Exploring</a>
     </div>
 </section>
 @endsection
