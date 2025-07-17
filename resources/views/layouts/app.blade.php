5c6696f4eb645c2aacb74af2d672303af6223bec
nehal5300 <ahmed15-5300@diu.edu.bd>
2025-07-17 21:44:07 +0600

    Converter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -345,6 +345,7 @@
             <li><a href="{{ url('/weather') }}">Weather Check</a></li>
             <li><a href="{{ url('/cart') }}">View Cart</a></li>
              <li><a href="{{ url('/Review') }}">Review</a></li>
+              <li><a href="{{ url('/Convert') }}">Currency Converter</a></li>
           </ul>
         </li>
 
